89ee3765c4cad8d92315d8303b6abe20c3895450
“adithi <adiwickramasekara@gmail.com>
2026-07-08 09:56:26 +0530

    Configured project startup and application entry point

---
+++ Memebr 2/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Windows.Forms;
+using ITAssetManagement.Forms;
+
+namespace ITAssetManagement
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginForm());
+        }
+    }
+}
